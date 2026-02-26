8b4eeb9b9823453132c2eb85c44d66a95e6bc059
D3N1S10 <daleckisdeniss@gmail.com>
2026-02-26 19:13:17 +0300

    Удалил вложенный git, теперь папка доступна

---
+++ PrIS-main/AdministrativalEmployee.java
@@ -0,0 +1,38 @@
+git rm -r --cached PrIS-main
+git add PrIS-main/
+git commit -m "Удалил вложенный git, теперь папка доступна"
+git push
+public class AdministrativalEmployee extends Employee {
+
+	/**
+	 * 
+	 * @param ssNo
+	 * @param name
+	 * @param email
+	 */
+	public AdministrativalEmployee(int ssNo, String name, String email) {
+		super(ssNo, name, email);
+	}
+
+	static class Main {
+		public static void main(String[] args) {
+			Employee admin = new AdministrativalEmployee(1111, "Назаров Денис", "nazarov@vsu.ru");
+			Faculty faculty = new Faculty(new AdministrativalEmployee(1, "Александр Крыловецкий", "fcs@vsu.ru"), "ФКН");
+			ResearchEmployee sciEmpl = new ResearchEmployee(2, "Дмитрий Соломатин", "solomatin@vsu.ru", "Программирование");
+			String adminString = admin.toString();
+			String facultyString = faculty.toString();
+			String sciEmplString = sciEmpl.toString();
+			System.out.println(adminString);
+			System.out.println(facultyString);
+			System.out.println(sciEmplString);
+			sciEmpl.setEmail("dsolomatin@vsu.ru");
+			admin.setSsNo(3);
+			sciEmpl.setFieldOfStudy("Java программирование");
+			faculty.setName("Факультет компьютерных наук");
+			System.out.println();
+			System.out.println(admin);
+			System.out.println(sciEmpl);
+			System.out.println(faculty);
+		}
+	}
+}
---
+++ PrIS-main/README.md
@@ -0,0 +1,6 @@
+# Далецкий Денис, 3 курс, 14.2 группа
+
+![image](https://github.com/user-attachments/assets/51edcef1-738b-4020-8f0d-977cc6376a1c)
+![image](https://github.com/user-attachments/assets/c1343755-a94c-43ef-aaa3-9c6569d1b379)
+![image](https://github.com/user-attachments/assets/0b4467ad-ff70-416c-a684-2b2dd295ff0a)
+![image](https://github.com/user-attachments/assets/95cb96b1-12ef-444f-8649-f28a909f7ddd)
